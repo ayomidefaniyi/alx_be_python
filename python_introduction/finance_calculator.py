bd392ce4bfbef338c2718d01dc1ce81ade948cc9
faniyi ayomideexit <faniyia25@gmail.com>
2025-11-16 19:30:54 +0100

    main

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,6 @@
+income = int(input("Enter your montly income "))
+expenses = int(input("Enter your total montly expenses "))
+savings = income - expenses
+print("Your monthly savings are", savings, ".")
+project = savings * 12 + (savings * 12 * 0.05)
+print("Projected savings after one year, with interest, is: ", project,".")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,5 @@
+hours = 2
+hour = 60 * 2
+minute = 60
+hours = hour * minute
+print("2 hours(s) is", hours, "seconds.")
