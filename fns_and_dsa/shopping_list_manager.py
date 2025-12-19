ba62a3c76d7037ad761bb781776da9072f8b916b
aydev <dd8081710@gmail.com>
2025-12-20 00:28:26 +0100

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,9 +1,9 @@
 def display_menu():
-    print("\nShopping List Manager")
-    print("1. Add Item")
-    print("2. Remove Item")
-    print("3. View List")
-    print("4. Exit")
+    print(f"\nShopping List Manager")
+    print(f"1. Add Item")
+    print(f"2. Remove Item")
+    print(f"3. View List")
+    print(f"4. Exit")
 
 def main():
     shopping_list = []
@@ -42,4 +42,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
